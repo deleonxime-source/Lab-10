5c06324db1fdccefb48414d39acde38893a22a18
Ximena De Leon <deleon.xime@gmail.com>
2026-03-30 23:51:01 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./Header";
+import Body from "./Body";
+import Footer from "./Footer";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
