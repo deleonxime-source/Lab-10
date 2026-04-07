871b012452d1614699db55203e32dc0ee4f6b420
Ximena De Leon <deleon.xime@gmail.com>
2026-04-06 19:10:45 -0500

    Add scopes to request access token

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,16 +6,19 @@ import "./App.css";
 import "./index.css";
 
 
-
 const config = {
-  signInRedirectURL: "https://lab-10-1-wi1f.onrender.com",
-  signOutRedirectURL: "https://lab-10-1-wi1f.onrender.com",
+  signInRedirectURL: "https://lab-10-1-wi1f.onrender.com/",
+  signOutRedirectURL: "https://lab-10-1-wi1f.onrender.com/",
   clientID: "7IM589V4c9vF96f_aDoGs46fLO8a",
   baseUrl: "https://api.asgardeo.io/t/fullstackweb",
+  scope: ["openid", "profile"]
 };
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider config={config}>
     <App />
   </AuthProvider>
 );
+
 
